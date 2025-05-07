bfc7df535fe7432e252fd85c91400e7a681977cd
saaad99 <ehdgnstla@naver.com>
2025-05-07 14:20:02 +0900

    [Test] 프론트와 API 통신 테스트용 컨트롤러 작성

---
+++ src/main/java/com/example/demo/controller/ApiTestController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class ApiTestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "API 통신 테스트";
+    }
+}

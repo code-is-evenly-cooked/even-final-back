08203d2a9682ff5eb06bc5747341225c9f10eaa9
saaad99 <ehdgnstla@naver.com>
2025-05-07 13:54:46 +0900

    [Chore] ApiResponse 응답 객체 추가

---
+++ src/main/java/com/example/demo/global/ApiResponse.java
@@ -0,0 +1,35 @@
+package com.example.demo.global;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ApiResponse<T> {
+
+    private boolean success;
+    private T data;
+    private String message;
+
+    private ApiResponse(boolean success, T data, String message) {
+        this.success = success;
+        this.data = data;
+        this.message = message;
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, null);
+    }
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return new ApiResponse<>(true, data, message);
+    }
+
+    public static <T> ApiResponse<T> fail(String message) {
+        return new ApiResponse<>(false, null, message);
+    }
+
+    public static <T> ApiResponse<T> fail(T data, String message) {
+        return new ApiResponse<>(false, data, message);
+    }
+}

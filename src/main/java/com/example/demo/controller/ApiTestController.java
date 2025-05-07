c932783f6b1f95da853734b7df459d6bfcbd1e2d
saaad99 <ehdgnstla@naver.com>
2025-05-07 16:27:13 +0900

    Test : ApiResponse 테스트 API 추가

--- src/main/java/com/example/demo/controller/ApiTestController.java
+++ src/main/java/com/example/demo/controller/ApiTestController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.global.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
@@ -11,7 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiTestController {
 
     @GetMapping("/test")
-    public String test() {
-        return "API 통신 테스트";
+    public ResponseEntity<ApiResponse<Map<String, Object>>> test() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", "동훈");
+        map.put("age", "27");
+
+        return ResponseEntity.ok(ApiResponse.success(map, "성공"));
     }
 }

74450693b2e54f67d6c321c345770516fae806eb
yoorym-kim <twilight99me@gmail.com>
2025-05-14 15:54:15 +0900

    feat : ProfileService, UserRepository 파일 추가 & getUserProfile 메서드 구현

---
+++ src/main/java/com/even/zaro/service/ProfileService.java
@@ -0,0 +1,35 @@
+package com.even.zaro.service;
+
+import com.even.zaro.dto.UserProfileDto;
+import com.even.zaro.entity.User;
+import com.even.zaro.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ProfileService {
+    private final UserRepository userRepository;
+
+    public UserProfileDto getUserProfile(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new IllegalArgumentException("User not found"));
+
+        String dDay = "D+500"; // 수정필요 (계산로직추가구현)
+        int postCount = 0; // 수정필요2 (추가구현)
+
+        return UserProfileDto.builder()
+                .userId(user.getId())
+                .nickname(user.getNickname())
+                .profileImage(user.getProfileImage())
+                .liveAloneDate(user.getLiveAloneDate())
+                .dDay(dDay)
+                .mbti(user.getMbti())
+                .postCount(postCount)
+                .followingCount(user.getFollowingCount())
+                .followerCount(user.getFollowerCount())
+                .build();
+    }
+}

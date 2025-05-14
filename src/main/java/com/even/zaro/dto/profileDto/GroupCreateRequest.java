7063a004da072c853d8d0bceff22b7fab003889c
saaad99 <ehdgnstla@naver.com>
2025-05-14 18:00:42 +0900

    Refactor : Group Dto 규칙 통일 - GroupCreate, GroupEdit 같이 Group뒤에 행동 명시

--- src/main/java/com/even/zaro/dto/profileDto/GroupCreateRequest.java
+++ src/main/java/com/even/zaro/dto/profileDto/GroupCreateRequest.java
@@ -3,7 +3,7 @@ package com.even.zaro.dto.profileDto;
 import lombok.Data;
 
 @Data
-public class CreateGroupRequest {
+public class GroupCreateRequest {
     private long userId;
     // Group 이름
     private String name;

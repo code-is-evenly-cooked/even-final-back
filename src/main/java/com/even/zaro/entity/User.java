f55cf56437a7bce0b08ec18e9e164cb6cc91c215
saaad99 <ehdgnstla@naver.com>
2025-05-14 15:12:37 +0900

    Feat : User Entity 객체 생성

---
+++ src/main/java/com/even/zaro/entity/User.java
@@ -0,0 +1,66 @@
+package com.even.zaro.entity;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column(name = "birth_date")
+    private LocalDate birthday;
+
+    @Column(name = "live_alone_date")
+    private LocalDate liveAloneDate;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "mbti")
+    private String mbti;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private Status status;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "last_login_at")
+    private LocalDateTime lastLoginAt;
+
+    @Column(name = "follower_count")
+    private int followerCount;
+
+    @Column(name = "following_count")
+    private int followingCount;
+}

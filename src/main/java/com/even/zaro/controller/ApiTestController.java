704574de965ce9d94138e5512146342ef3fbc6fb
saaad99 <ehdgnstla@naver.com>
2025-05-14 11:14:09 +0900

    Chore : 예외 테스트 API 추가

--- src/main/java/com/even/zaro/controller/ApiTestController.java
+++ src/main/java/com/even/zaro/controller/ApiTestController.java
@@ -2,6 +2,8 @@ package com.even.zaro.controller;
 
 import com.even.zaro.dto.ExDTO;
 import com.even.zaro.global.ApiResponse;
+import com.even.zaro.global.exception.exampleEx.ExampleException;
+import com.even.zaro.global.exception.userEx.UserException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
@@ -25,5 +27,17 @@ public class ApiTestController {
                 .build();
 
         return ResponseEntity.ok(ApiResponse.success("API 테스트 성공~", exDto));
+    }
+
+    @Operation(summary = "예외 테스트 API", description = "예외 테스트 응답 확인용")
+    @GetMapping("/error-throw")
+    public ResponseEntity<ApiResponse<?>> errorThrow() {
+        throw UserException.NotFoundUserException();
+    }
+
+    @Operation(summary = "예외 테스트 API", description = "예외 테스트 응답 확인용")
+    @GetMapping("/error-throw/ex")
+    public ResponseEntity<ApiResponse<?>> errorThrowEx() {
+        throw ExampleException.NotFoundExampleException();
     }
 }

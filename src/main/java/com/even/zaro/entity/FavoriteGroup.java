862ee7722e4dad22ec8d5c6a2d3d9256519129f9
saaad99 <ehdgnstla@naver.com>
2025-05-14 15:42:25 +0900

    Feat : FavoriteGroup 객체 생성

---
+++ src/main/java/com/even/zaro/entity/FavoriteGroup.java
@@ -0,0 +1,42 @@
+package com.even.zaro.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "favorite_group")
+public class FavoriteGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", unique = true, nullable = false)
+    private User user;
+
+    // Group 이름
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}

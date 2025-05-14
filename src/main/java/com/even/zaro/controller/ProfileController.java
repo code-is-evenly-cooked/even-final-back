e9c4fb04f2d711b2f20b28a4ecab707777ac397d
saaad99 <ehdgnstla@naver.com>
2025-05-14 16:33:40 +0900

    Feat : 즐겨찾기 추가 API 구현

--- src/main/java/com/even/zaro/controller/ProfileController.java
+++ src/main/java/com/even/zaro/controller/ProfileController.java
@@ -1,4 +1,29 @@
 package com.even.zaro.controller;
 
+import com.even.zaro.dto.profileDto.CreateGroupRequest;
+import com.even.zaro.global.ApiResponse;
+import com.even.zaro.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/profile")
+@Slf4j
+@RequiredArgsConstructor
 public class ProfileController {
+    private final ProfileService profileService;
+
+    @PostMapping("/group")
+    public ResponseEntity<ApiResponse<String>> createGroup(@RequestBody CreateGroupRequest request) {
+        profileService.createGroup(request);
+
+        return ResponseEntity.ok(ApiResponse.success("성공적으로 그룹이 생성되었습니다."));
+    }
+
 }
--- src/main/java/com/even/zaro/service/ProfileService.java
+++ src/main/java/com/even/zaro/service/ProfileService.java
@@ -1,4 +1,46 @@
 package com.even.zaro.service;
 
+import com.even.zaro.dto.profileDto.CreateGroupRequest;
+import com.even.zaro.entity.Favorite;
+import com.even.zaro.entity.FavoriteGroup;
+import com.even.zaro.entity.User;
+import com.even.zaro.global.ApiResponse;
+import com.even.zaro.global.exception.userEx.UserException;
+import com.even.zaro.repository.FavoriteGroupRepository;
+import com.even.zaro.repository.FavoriteRepository;
+import com.even.zaro.repository.PlaceRepository;
+import com.even.zaro.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+
+@Service
+@Slf4j
+@AllArgsConstructor
 public class ProfileService {
+    private final FavoriteRepository favoriteRepository;
+    private final UserRepository userRepository;
+    private final PlaceRepository placeRepository;
+    private final FavoriteGroupRepository favoriteGroupRepository;
+
+    @Transactional
+    public void createGroup(CreateGroupRequest request) {
+
+        User user = userRepository.findById(request.getUserId())
+                .orElseThrow(UserException::NotFoundUserException);
+
+        FavoriteGroup favoriteGroup = FavoriteGroup.builder()
+                .user(user) // 유저 설정
+                .name(request.getName()) // Group 이름 설정
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        favoriteGroupRepository.save(favoriteGroup);
+    }
 }

764a88c0580642bfcb8e30956132643ba311f4e5
yoorym-kim <twilight99me@gmail.com>
2025-05-14 19:57:55 +0900

    feat : PostLike 엔티티 클래스 생성

---
+++ src/main/java/com/even/zaro/entity/PostLike.java
@@ -0,0 +1,29 @@
+package com.even.zaro.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post_like")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}

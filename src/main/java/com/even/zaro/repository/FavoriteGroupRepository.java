e3dd9454f667111055c24de72516820109292987
saaad99 <ehdgnstla@naver.com>
2025-05-14 17:15:45 +0900

    feature : FavoriteGroup List 반환 메서드 정의

--- src/main/java/com/even/zaro/repository/FavoriteGroupRepository.java
+++ src/main/java/com/even/zaro/repository/FavoriteGroupRepository.java
@@ -1,7 +1,11 @@
 package com.even.zaro.repository;
 
 import com.even.zaro.entity.FavoriteGroup;
+import com.even.zaro.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FavoriteGroupRepository extends JpaRepository<FavoriteGroup, Long> {
+    List<FavoriteGroup> findByUser(User user);
 }

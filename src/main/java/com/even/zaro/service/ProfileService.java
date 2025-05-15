6fbaed80dbbeb890079e1552127d3dfecee2a090
saaad99 <ehdgnstla@naver.com>
2025-05-15 12:47:52 +0900

    Comment : 불필요한 주석 제거

--- src/main/java/com/even/zaro/service/ProfileService.java
+++ src/main/java/com/even/zaro/service/ProfileService.java
@@ -18,18 +18,14 @@ import com.even.zaro.repository.PostLikeRepository;
 import com.even.zaro.repository.PostRepository;
 import com.even.zaro.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j

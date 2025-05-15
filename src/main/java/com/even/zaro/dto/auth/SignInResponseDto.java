e01bf330c5b2df16ba59100790315947cda30e1c
nahyun <knahyuk@gmail.com>
2025-05-15 15:37:59 +0900

    fix: 이전 commit 누락

--- src/main/java/com/even/zaro/dto/auth/SignInResponseDto.java
+++ src/main/java/com/even/zaro/dto/auth/SignInResponseDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class LoginResponseDto {
+public class SignInResponseDto {
     private String accessToken;
     private String refreshToken;
     private Long userId;

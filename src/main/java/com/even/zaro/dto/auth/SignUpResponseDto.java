2e9d4c1b0ff94f7dacc028f638b24937a168f7c4
nahyun <knahyuk@gmail.com>
2025-05-15 10:59:55 +0900

    add: signup 응답 dto에 전체 필드 생성자 애노테이션 추가

--- src/main/java/com/even/zaro/dto/auth/SignUpResponseDto.java
+++ src/main/java/com/even/zaro/dto/auth/SignUpResponseDto.java
@@ -1,5 +1,10 @@
 package com.even.zaro.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class SignUpResponseDto {
     private Long id;
     private String email;

e4245b4f78571d6fab4e5ed489144002a3fccf94
nahyun <knahyuk@gmail.com>
2025-05-15 15:16:29 +0900

    feat: 로그인 요청 응답 dto

---
+++ src/main/java/com/even/zaro/dto/auth/LoginResponseDto.java
@@ -0,0 +1,15 @@
+package com.even.zaro.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponseDto {
+    private String accessToken;
+    private String refreshToken;
+    private Long userId;
+    private String email;
+    private String nickname;
+    private String profileImageUrl;
+}

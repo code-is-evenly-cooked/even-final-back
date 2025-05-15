1fba11e5656a71663ef0af959847a671f8cdad2a
saaad99 <ehdgnstla@naver.com>
2025-05-15 17:05:58 +0900

    Refactor : SwaggerUI 문서화 작업

--- src/main/java/com/even/zaro/dto/favoriteDTO/FavoriteResponse.java
+++ src/main/java/com/even/zaro/dto/favoriteDTO/FavoriteResponse.java
@@ -1,8 +1,6 @@
 package com.even.zaro.dto.favoriteDTO;
 
-import com.even.zaro.entity.FavoriteGroup;
-import com.even.zaro.entity.Place;
-import com.even.zaro.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,23 +9,34 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class FavoriteResponse {
+
+    @Schema(description = "즐겨찾기 id", example = "1")
     private long id;
 
+    @Schema(description = "유저 id", example = "1")
     private long userId;
 
+    @Schema(description = "그룹 id", example = "1")
     private long groupId;
 
+    @Schema(description = "장소 id", example = "1")
     private long placeId;
 
+    @Schema(description = "장소 id", example = "37.5665")
     private double lat;
 
+    @Schema(description = "장소 id", example = "126.978")
     private double lng;
 
+    @Schema(description = "즐겨찾기 메모", example = "주말에 친구랑 가보고 싶은 감성카페")
     private String memo;
 
+    @Schema(description = "생성 시간", example = "2025-05-15 16:13:10")
     private LocalDateTime createdAt;
 
+    @Schema(description = "수정 시간", example = "2025-05-15 16:13:10")
     private LocalDateTime updatedAt;
 
+    @Schema(description = "삭제 여부", example = "0")
     private boolean isDeleted;
 }

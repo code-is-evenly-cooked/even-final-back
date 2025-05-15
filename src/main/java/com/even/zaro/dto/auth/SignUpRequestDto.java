3a2cd03637bb6f35f55064db72bdb6d34bab2154
nahyun <knahyuk@gmail.com>
2025-05-15 10:30:33 +0900

    fix: 서비스에서 예외처리로 애노테이션 삭제

--- src/main/java/com/even/zaro/dto/auth/SignUpRequestDto.java
+++ src/main/java/com/even/zaro/dto/auth/SignUpRequestDto.java
@@ -1,13 +1,13 @@
 package com.even.zaro.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 public class SignUpRequestDto {
-    @NotBlank
     private String email;
-
-    @NotBlank
     private String nickname;
-
     private String password;
 }

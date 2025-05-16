1b4ab9a6212218cf4ff04280e031d2d246779c57
yoorym-kim <twilight99me@gmail.com>
2025-05-16 14:34:05 +0900

    feat : CommentRepository 인터페이스 생성

---
+++ src/main/java/com/even/zaro/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.even.zaro.repository;
+
+import com.even.zaro.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}

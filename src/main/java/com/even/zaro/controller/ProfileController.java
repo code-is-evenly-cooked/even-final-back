c463a36a20e77b9fbac810e9fa6a794d8a849300
saaad99 <ehdgnstla@naver.com>
2025-05-16 18:57:16 +0900

    Ref :  프로필 API 에서 그룹, 즐겨찾기 API 분리 - 필요없는 주석 제거 - API 분리

--- src/main/java/com/even/zaro/controller/ProfileController.java
+++ src/main/java/com/even/zaro/controller/ProfileController.java
@@ -2,29 +2,16 @@ package com.even.zaro.controller;
 
 import com.even.zaro.dto.UserPostDto;
 import com.even.zaro.dto.UserProfileDto;
-import com.even.zaro.dto.favorite.FavoriteAddRequest;
-import com.even.zaro.dto.favorite.FavoriteAddResponse;
-import com.even.zaro.dto.favorite.FavoriteEditRequest;
-import com.even.zaro.dto.favorite.FavoriteResponse;
-import com.even.zaro.dto.jwt.JwtUserInfoDto;
 import com.even.zaro.global.ApiResponse;
 import com.even.zaro.service.ProfileService;
-import com.nimbusds.openid.connect.sdk.claims.UserInfo;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import com.even.zaro.dto.profile.GroupCreateRequest;
-import com.even.zaro.dto.profile.GroupEditRequest;
-import com.even.zaro.dto.profile.GroupResponse;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -70,58 +57,5 @@ public class ProfileController {
                 "content", likedPosts.getContent(),
                 "totalPages", likedPosts.getTotalPages()
         )));
-    }
-
-
-
-    // -------------------------------------------------------------------------------------------------- //
-
-    @Operation(summary = "사용자의 그룹 리스트 조회", description = "해당 유저의 그룹 리스트를 조회합니다.", security = {@SecurityRequirement(name = "bearer-key")})
-    @GetMapping("/user/{userId}/group")
-    public ResponseEntity<ApiResponse<List<GroupResponse>>> getFavoriteGroupsByUserId (
-            @PathVariable("userId") long userId,
-            @AuthenticationPrincipal JwtUserInfoDto userInfoDto) {
-        List<GroupResponse> groupList = profileService.getFavoriteGroups(userId);
-
-        return ResponseEntity.ok(ApiResponse.success("해당 유저의 즐겨찾기 그룹 리스트를 조회했습니다.", groupList));
-    }
-
-
-    @Operation(summary = "나의 즐겨찾기 그룹 리스트 조회", description = "나의 즐겨찾기 그룹 리스트를 조회합니다.", security = {@SecurityRequirement(name = "bearer-key")})
-    @GetMapping("/group")
-    public ResponseEntity<ApiResponse<List<GroupResponse>>> getMyFavoriteGroups(
-            @AuthenticationPrincipal JwtUserInfoDto userInfoDto) {
-        List<GroupResponse> groupList = profileService.getFavoriteGroups(userInfoDto.getUserId());
-
-        return ResponseEntity.ok(ApiResponse.success("나의 즐겨찾기 그룹 리스트를 조회했습니다.", groupList));
-    }
-
-    @Operation(summary = "즐겨찾기 그룹 추가", description = "그룹 이름을 입력받아 그룹을 추가합니다.")
-    @PostMapping("/group")
-    public ResponseEntity<ApiResponse<String>> createGroup(@RequestBody GroupCreateRequest request,
-                                                           @AuthenticationPrincipal JwtUserInfoDto userInfoDto) {
-        long userId = userInfoDto.getUserId();
-        profileService.createGroup(request, userId);
-
-        return ResponseEntity.ok(ApiResponse.success("성공적으로 즐겨찾기 그룹이 생성되었습니다."));
-    }
-
-    @Operation(summary = "즐겨찾기 그룹 삭제", description = "groupId로 즐겨찾기 그룹 리스트를 삭제합니다.", security = {@SecurityRequirement(name = "bearer-key")})
-    @DeleteMapping("/group/{groupId}")
-    public ResponseEntity<ApiResponse<String>> deleteGroup(@PathVariable("groupId") long groupId,
-                                                           @AuthenticationPrincipal JwtUserInfoDto userInfoDto) {
-        profileService.deleteGroup(groupId, userInfoDto.getUserId());
-
-        return ResponseEntity.ok(ApiResponse.success("성공적으로 그룹을 삭제했습니다."));
-    }
-
-    @Operation(summary = "즐겨찾기 그룹 수정", description = "그룹 이름을 수정합니다.")
-    @PatchMapping("/group/{groupId}")
-    public ResponseEntity<ApiResponse<String>> editGroup(@PathVariable("groupId") long groupId,
-                                                         @RequestBody GroupEditRequest request,
-                                                         @AuthenticationPrincipal JwtUserInfoDto userInfoDto) {
-        profileService.editGroup(groupId, request, userInfoDto.getUserId());
-
-        return ResponseEntity.ok(ApiResponse.success("성공적으로 그룹 이름을 수정했습니다."));
     }
 }
--- src/main/java/com/even/zaro/service/ProfileService.java
+++ src/main/java/com/even/zaro/service/ProfileService.java
@@ -2,22 +2,9 @@ package com.even.zaro.service;
 
 import com.even.zaro.dto.UserPostDto;
 import com.even.zaro.dto.UserProfileDto;
-import com.even.zaro.dto.favorite.FavoriteAddRequest;
-import com.even.zaro.dto.favorite.FavoriteAddResponse;
-import com.even.zaro.dto.favorite.FavoriteEditRequest;
-import com.even.zaro.dto.favorite.FavoriteResponse;
-import com.even.zaro.dto.profile.GroupCreateRequest;
-import com.even.zaro.dto.profile.GroupEditRequest;
-import com.even.zaro.dto.profile.GroupResponse;
-import com.even.zaro.entity.Favorite;
-import com.even.zaro.entity.FavoriteGroup;
-import com.even.zaro.entity.Place;
 import com.even.zaro.entity.User;
 import com.even.zaro.global.ErrorCode;
 import com.even.zaro.global.exception.CustomException;
-import com.even.zaro.global.exception.map.MapException;
-import com.even.zaro.global.exception.profile.ProfileException;
-import com.even.zaro.global.exception.user.UserException;
 import com.even.zaro.repository.*;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,19 +13,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
 @Service
 @Slf4j
 @AllArgsConstructor
 @Transactional
 public class ProfileService {
-    private final FavoriteRepository favoriteRepository;
     private final UserRepository userRepository;
-    private final PlaceRepository placeRepository;
-    private final FavoriteGroupRepository favoriteGroupRepository;
     private final PostRepository postRepository;
     private final PostLikeRepository postLikeRepository;
 
@@ -99,88 +79,7 @@ public class ProfileService {
                         .build());
     }
 
-    public void createGroup(GroupCreateRequest request, long userid) {
 
-        User user = userRepository.findById(userid).orElseThrow(() -> new UserException(ErrorCode.EXAMPLE_USER_NOT_FOUND));
-
-        boolean dupCheck = groupNameDuplicateCheck(request.getName(), userid);
-
-        // 해당 유저가 이미 있는 그룹 이름을 입력했을 때
-        if (dupCheck) {
-            throw new ProfileException(ErrorCode.GROUP_ALREADY_EXIST);
-        }
-
-        FavoriteGroup favoriteGroup = FavoriteGroup.builder().user(user) // 유저 설정
-                .name(request.getName()) // Group 이름 설정
-                .updatedAt(LocalDateTime.now()).build();
-
-        favoriteGroupRepository.save(favoriteGroup);
-    }
-
-    @Transactional(readOnly = true)
-    public List<GroupResponse> getFavoriteGroups(long userId) {
-
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserException(ErrorCode.EXAMPLE_USER_NOT_FOUND));
-
-        // userId 값이 일치하는 데이터 조회
-        List<FavoriteGroup> groupList = favoriteGroupRepository.findByUser(user);
-
-        // GroupResponse 리스트로 변환
-        List<GroupResponse> responseList = groupList.stream().map(group ->
-                GroupResponse.builder()
-                        .id(group.getId())
-                        .name(group.getName())
-                        .build())
-                .toList();
-
-        return responseList;
-    }
-
-    public void deleteGroup(long groupId, long userId) {
-        FavoriteGroup group = favoriteGroupRepository.findById(groupId)
-                .orElseThrow(() -> new ProfileException(ErrorCode.GROUP_NOT_FOUND));
-
-        // 다른 사용자의 그룹 삭제 방지
-        if (group.getUser().getId() != userId) {
-            throw new ProfileException(ErrorCode.UNAUTHORIZED_GROUP_DELETE);
-        }
-
-        // 이미 삭제 처리 된 경우
-        if (group.isDeleted()) {
-            throw new ProfileException(ErrorCode.GROUP_ALREADY_DELETE);
-        }
-
-        group.setDeleted(true);
-
-        favoriteGroupRepository.save(group);
-    }
-
-    public void editGroup(long groupId, GroupEditRequest request, long userId) {
-        FavoriteGroup group = favoriteGroupRepository.findById(groupId)
-                .orElseThrow(() -> new ProfileException(ErrorCode.GROUP_NOT_FOUND));
-
-        // 다른 사용자의 즐겨찾기 그룹 수정 방지
-        if (group.getUser().getId() != userId) {
-            throw new ProfileException(ErrorCode.UNAUTHORIZED_GROUP_UPDATE);
-        }
-
-        boolean dupCheck = groupNameDuplicateCheck(request.getName(), group.getUser().getId());
-
-        // 해당 유저가 이미 있는 즐겨찾기 그룹 이름을 입력했을 때
-        if (dupCheck) {
-            throw new ProfileException(ErrorCode.GROUP_ALREADY_EXIST);
-        }
-        group.setName(request.getName());
-
-        favoriteGroupRepository.save(group);
-    }
-
-
-    // 입력한 그룹 이름이 이미 해당 userId가 가지고 있는지 확인
-    public boolean groupNameDuplicateCheck(String groupName, long userId) {
-        return favoriteGroupRepository.existsByUser_IdAndName(userId, groupName);
-    }
 
 
 

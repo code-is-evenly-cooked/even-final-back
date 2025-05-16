40f5c7f8601f3c75834b738259b889fde9d45616
yoorym-kim <twilight99me@gmail.com>
2025-05-16 17:00:18 +0900

    feat : FollowerFollowingListDto 구성 추가

--- src/main/java/com/even/zaro/dto/profile/FollowerFollowingListDto.java
+++ src/main/java/com/even/zaro/dto/profile/FollowerFollowingListDto.java
@@ -1,4 +1,16 @@
 package com.even.zaro.dto.profile;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FollowerFollowingListDto {
+    private Long userId;
+    private String userName;
+    private String profileImage;
 }

319e6437fff86e6b0ec6521217914306496b7d89
nahyun <knahyuk@gmail.com>
2025-05-16 14:22:06 +0900

    add: 메일 전송 관련 에러코드 추가

--- src/main/java/com/even/zaro/global/ErrorCode.java
+++ src/main/java/com/even/zaro/global/ErrorCode.java
@@ -30,6 +30,11 @@ public enum ErrorCode {
     EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "가입된 이메일이 아닙니다."),
     INCORRECT_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
 
+        // 이메일 인증
+    EMAIL_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "유효하지 않은 인증 요청입니다."),
+    EMAIL_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "인증 유효시간이 만료되었습니다."),
+    EMAIL_TOKEN_ALREADY_VERIFIED(HttpStatus.BAD_REQUEST, "이미 인증이 완료된 요청입니다."),
+    EMAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이메일 전송에 실패했습니다."),
     // 게시글 Post
 
     // 프로필 Profile

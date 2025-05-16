89926ec72065d236484beba4aef53b392d424987
nahyun <knahyuk@gmail.com>
2025-05-16 22:22:50 +0900

    fix: 애노테이션 추가

--- src/main/java/com/even/zaro/repository/EmailTokenRepository.java
+++ src/main/java/com/even/zaro/repository/EmailTokenRepository.java
@@ -3,9 +3,11 @@ package com.even.zaro.repository;
 import com.even.zaro.entity.EmailToken;
 import com.even.zaro.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface EmailTokenRepository extends JpaRepository<EmailToken, Long> {
     Optional<EmailToken> findByToken(String token);
 

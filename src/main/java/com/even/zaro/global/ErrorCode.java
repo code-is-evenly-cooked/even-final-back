a3ff2d1acfd0c8219944cbb45e3b0bd665b2e637
nahyun <knahyuk@gmail.com>
2025-05-16 22:43:59 +0900

    add: 리프레쉬 토큰 관련 에러코드 추가

--- src/main/java/com/even/zaro/global/ErrorCode.java
+++ src/main/java/com/even/zaro/global/ErrorCode.java
@@ -35,6 +35,11 @@ public enum ErrorCode {
     EMAIL_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "인증 유효시간이 만료되었습니다."),
     EMAIL_TOKEN_ALREADY_VERIFIED(HttpStatus.BAD_REQUEST, "이미 인증이 완료된 요청입니다."),
     EMAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이메일 전송에 실패했습니다."),
+
+        //jwt
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "리프레쉬 토큰을 찾을 수 없습니다."),
+
     // 게시글 Post
 
     // 프로필 Profile

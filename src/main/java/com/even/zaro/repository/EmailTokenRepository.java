c3575c1ea20713289aeaa1e5487953da574b996f
nahyun <knahyuk@gmail.com>
2025-05-16 15:02:59 +0900

    feat: 메일 repo row 삭제 쿼리 추가

--- src/main/java/com/even/zaro/repository/EmailTokenRepository.java
+++ src/main/java/com/even/zaro/repository/EmailTokenRepository.java
@@ -1,10 +1,13 @@
 package com.even.zaro.repository;
 
 import com.even.zaro.entity.EmailToken;
+import com.even.zaro.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface EmailTokenRepository extends JpaRepository<EmailToken, Long> {
     Optional<EmailToken> findByToken(String token);
+
+    void deleteByUser(User user);
 }

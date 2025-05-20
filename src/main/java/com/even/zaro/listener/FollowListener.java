14b5386c672e6472ab4db10f695810d41bd07fa2
yoorym-kim <twilight99me@gmail.com>
2025-05-20 14:52:46 +0900

    feat : FollowListener 클래스 추가, Follow 엔티티에 @EntityListeners 추가

---
+++ src/main/java/com/even/zaro/listener/FollowListener.java
@@ -0,0 +1,9 @@
+package com.even.zaro.listener;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FollowListener {
+}

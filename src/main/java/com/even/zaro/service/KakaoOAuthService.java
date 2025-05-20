dd73280bb47e7e0c750e4c9877aac4a295d41e72
nahyun <knahyuk@gmail.com>
2025-05-20 19:09:57 +0900

    feat: 올바르지 않은 토큰 예외처리 추가

--- src/main/java/com/even/zaro/service/KakaoOAuthService.java
+++ src/main/java/com/even/zaro/service/KakaoOAuthService.java
@@ -1,11 +1,13 @@
 package com.even.zaro.service;
 
 import com.even.zaro.dto.auth.KakaoUserInfoDto;
+import com.even.zaro.global.ErrorCode;
+import com.even.zaro.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-
 
 @Component
 @RequiredArgsConstructor
@@ -16,17 +18,21 @@ public class KakaoOAuthService {
     public KakaoUserInfoDto getUserInfo(String kakaoAccessToken) {
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(kakaoAccessToken);
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         HttpEntity<Void> entity = new HttpEntity<>(headers);
 
-        ResponseEntity<KakaoUserInfoDto> response = restTemplate.exchange(
-                "https://kapi.kakao.com/v2/user/me",
-                HttpMethod.GET,
-                entity,
-                KakaoUserInfoDto.class
-        );
+        try {
+            ResponseEntity<KakaoUserInfoDto> response = restTemplate.exchange(
+                    "https://kapi.kakao.com/v2/user/me",
+                    HttpMethod.GET,
+                    entity,
+                    KakaoUserInfoDto.class
+            );
 
-        return response.getBody();
+            return response.getBody();
+        } catch (HttpClientErrorException e) {
+            throw new CustomException(ErrorCode.INVALID_OAUTH_TOKEN);
+        }
+
     }
 }

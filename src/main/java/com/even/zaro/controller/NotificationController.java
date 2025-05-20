4825c87b42ef0730a6d8a6212814cc00fb2a7c5c
yoorym-kim <twilight99me@gmail.com>
2025-05-20 12:22:55 +0900

    feat : NotificationController 추가

---
+++ src/main/java/com/even/zaro/controller/NotificationController.java
@@ -0,0 +1,15 @@
+package com.even.zaro.controller;
+
+import com.even.zaro.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/notifications")
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+}

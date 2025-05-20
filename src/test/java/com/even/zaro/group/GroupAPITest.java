340d208052d0f435a797d4dce5401cdb38309b62
saaad99 <ehdgnstla@naver.com>
2025-05-20 16:01:34 +0900

    Test : 해당_사용자의_그룹리스트_조회_성공테스트 생성

--- src/test/java/com/even/zaro/group/GroupAPITest.java
+++ src/test/java/com/even/zaro/group/GroupAPITest.java
@@ -1,13 +1,24 @@
 package com.even.zaro.group;
 
 import com.even.zaro.controller.GroupController;
+import com.even.zaro.dto.group.GroupResponse;
+import com.even.zaro.entity.FavoriteGroup;
+import com.even.zaro.entity.Provider;
+import com.even.zaro.entity.Status;
+import com.even.zaro.entity.User;
 import com.even.zaro.repository.FavoriteGroupRepository;
 import com.even.zaro.repository.UserRepository;
 import com.even.zaro.service.GroupService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -15,15 +26,45 @@ import org.springframework.transaction.annotation.Transactional;
 public class GroupAPITest {
 
     @Autowired
-    GroupController groupController;
-
-    @Autowired
     GroupService groupService;
 
+
+    // repository --------
     @Autowired
     UserRepository userRepository;
 
     @Autowired
     FavoriteGroupRepository favoriteGroupRepository;
+
+    @Test
+    void 해당_사용자의_그룹리스트_조회_성공테스트() {
+
+        // Given
+        User user = userRepository.save(User.builder()
+                .email("test@example.com")
+                .password("Password1234!")
+                .nickname("테스트유저")
+                .provider(Provider.LOCAL)
+                .status(Status.PENDING)
+                .build());
+
+        // 즐겨찾기 그룹 예시 데이터
+        FavoriteGroup group1 = FavoriteGroup.builder().user(user).name("맛집 모음").build();
+        FavoriteGroup group2 = FavoriteGroup.builder().user(user).name("데이트 코스").build();
+        FavoriteGroup group3 = FavoriteGroup.builder().user(user).name("가보고 싶은 곳").build();
+
+        favoriteGroupRepository.saveAll(List.of(group1, group2, group3));// 한꺼번에 그룹 리스트 저장
+
+        // When
+        List<GroupResponse> favoriteGroups = groupService.getFavoriteGroups(user.getId());
+
+        // Then
+        assertThat(favoriteGroups.size()).isEqualTo(3); // 개수 검증
+        assertThat(favoriteGroups.stream().map(GroupResponse::getName) // 그룹 이름 리스트 검증
+                .toList()).containsExactlyInAnyOrder("맛집 모음", "데이트 코스", "가보고 싶은 곳");
+    }
+
+
+
 
 }

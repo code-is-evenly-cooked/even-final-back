0ef3bbdba80b6c91316679ccde7f0523f3353fef
saaad99 <ehdgnstla@naver.com>
2025-05-20 12:20:24 +0900

    Ref : @EntityGraph 를 이용해 기존 코드에서 발생할 수 있는 N + 1 쿼리 문제를 개선

--- src/main/java/com/even/zaro/repository/FavoriteRepository.java
+++ src/main/java/com/even/zaro/repository/FavoriteRepository.java
@@ -4,12 +4,15 @@ import com.even.zaro.entity.Favorite;
 import com.even.zaro.entity.FavoriteGroup;
 import com.even.zaro.entity.Place;
 import com.even.zaro.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     List<Favorite> findAllByGroup(FavoriteGroup group);
+
+    @EntityGraph(attributePaths = { "user"})
     List<Favorite> findAllByPlace(Place place);
     List<Favorite> findByPlaceAndUser(Place place, User user);
 }

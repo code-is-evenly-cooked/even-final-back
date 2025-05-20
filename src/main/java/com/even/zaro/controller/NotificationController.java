9b3026719c432975e1f0060e659434cf5b94b1df
yoorym-kim <twilight99me@gmail.com>
2025-05-20 18:10:34 +0900

    Swagger : NotificationController에 스웨거 문서화 코드 추가

--- src/main/java/com/even/zaro/controller/NotificationController.java
+++ src/main/java/com/even/zaro/controller/NotificationController.java
@@ -4,6 +4,9 @@ import com.even.zaro.dto.jwt.JwtUserInfoDto;
 import com.even.zaro.dto.notification.NotificationDto;
 import com.even.zaro.global.ApiResponse;
 import com.even.zaro.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +21,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/notifications")
 @RequiredArgsConstructor
+@Tag(name = "알림", description = "로그인 된 사용자 알림 API")
 public class NotificationController {
 
     private final NotificationService notificationService;
 
+    @Operation(
+            summary = "유저 알림 list 조회 (인증 필요)",
+            description = "로그인 된 유저의 알림 목록을 조회합니다.",
+            security = {@SecurityRequirement(name = "bearer-key")})
     @GetMapping
     public ResponseEntity<?> getNotificationsList(
             @AuthenticationPrincipal JwtUserInfoDto userInfoDto) {

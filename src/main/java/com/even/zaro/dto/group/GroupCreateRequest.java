68c33b06fc485cbeb353a69e38db8e61653aa09e
saaad99 <ehdgnstla@naver.com>
2025-05-20 16:07:00 +0900

    Ref : @Data -> @Getter로 개선

--- src/main/java/com/even/zaro/dto/group/GroupCreateRequest.java
+++ src/main/java/com/even/zaro/dto/group/GroupCreateRequest.java
@@ -2,8 +2,9 @@ package com.even.zaro.dto.group;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 public class GroupCreateRequest {
     // Group 이름
     @Schema(description = "그룹 이름", example = "강릉 맛집!!")

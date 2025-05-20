47e54690492477f962466bfc213f23b4745622e5
saaad99 <ehdgnstla@naver.com>
2025-05-20 20:57:57 +0900

    Ref : FavoriteAddRequest @Builder 추가

--- src/main/java/com/even/zaro/dto/favorite/FavoriteAddRequest.java
+++ src/main/java/com/even/zaro/dto/favorite/FavoriteAddRequest.java
@@ -1,9 +1,11 @@
 package com.even.zaro.dto.favorite;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class FavoriteAddRequest {
     @Schema(description = "장소 ID", example = "3")
     private long placeId;

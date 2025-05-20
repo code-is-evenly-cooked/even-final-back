b4d13557ecc75321bd95685b8ba543cbfefb4a6b
nahyun <knahyuk@gmail.com>
2025-05-20 17:02:41 +0900

    feat: db 저장 전 카카오 정보 받아오는 dto

---
+++ src/main/java/com/even/zaro/dto/auth/KakaoUserInfoDto.java
@@ -0,0 +1,31 @@
+package com.even.zaro.dto.auth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class KakaoUserInfoDto {
+    private Long id;
+    @JsonProperty("kakao_account")
+    private KakaoAccount kakaoAccount;
+
+    @Getter
+    @NoArgsConstructor
+    public static class KakaoAccount {
+        private Profile profile;
+        private String email;
+        private String gender;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Profile {
+        private String nickname;
+        @JsonProperty("profile_image_url")
+        private String profileImageUrl;
+    }
+}

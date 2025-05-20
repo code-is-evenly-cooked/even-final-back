da69fe6494b65b67ed38adfa1f1356bc71e8ba59
saaad99 <ehdgnstla@naver.com>
2025-05-20 18:33:23 +0900

    Test : GroupException 수정 - 에러 코드 조회 위해 필드 추가 - @Getter 추가

--- src/main/java/com/even/zaro/global/exception/group/GroupException.java
+++ src/main/java/com/even/zaro/global/exception/group/GroupException.java
@@ -2,9 +2,16 @@ package com.even.zaro.global.exception.group;
 
 import com.even.zaro.global.ErrorCode;
 import com.even.zaro.global.exception.CustomException;
+import lombok.Getter;
 
+@Getter
 public class GroupException extends CustomException {
+    private final ErrorCode errorCode;
+
     public GroupException(ErrorCode errorCode) {
         super(errorCode);
+        this.errorCode = errorCode;
     }
+
+
 }

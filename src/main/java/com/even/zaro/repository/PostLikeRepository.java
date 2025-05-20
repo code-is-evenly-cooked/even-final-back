4dfe94396071e48dc9dc54587227f918bc434b88
LJIEUN <eun773c@daum.net>
2025-05-21 00:56:57 +0900

    feat: 게시글 좋아요 Repository 추가

--- src/main/java/com/even/zaro/repository/PostLikeRepository.java
+++ src/main/java/com/even/zaro/repository/PostLikeRepository.java
@@ -7,7 +7,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
     Page<PostLike> findByUser(User user, Pageable pageable);
+    Optional<PostLike> findByUserIdAndPostId(Long userId, Long postId);
+    boolean existsByUserIdAndPostId(Long userId, Long postId);
+    List<PostLike> findAllByUser(Long userId);
 }

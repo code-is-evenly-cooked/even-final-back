e50131c14d50dc7b91b7008681e9b3079e5d984b
saaad99 <ehdgnstla@naver.com>
2025-05-20 16:16:31 +0900

    Ref : Builder 추가 - 기존 @Data 어노테이션 제거 - 테스트 위해 @Builder 추가

--- src/main/java/com/even/zaro/dto/group/GroupCreateRequest.java
+++ src/main/java/com/even/zaro/dto/group/GroupCreateRequest.java
@@ -1,10 +1,11 @@
 package com.even.zaro.dto.group;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class GroupCreateRequest {
     // Group 이름
     @Schema(description = "그룹 이름", example = "강릉 맛집!!")

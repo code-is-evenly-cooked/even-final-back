cd90888c6b35399d7950ac5e450f02dd7661db4d
nahyun <knahyuk@gmail.com>
2025-05-21 16:19:00 +0900

    feat: user repo에 닉네임으로 유저 찾는 메서드 추가

--- src/main/java/com/even/zaro/repository/UserRepository.java
+++ src/main/java/com/even/zaro/repository/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByProviderAndProviderId(Provider provider, String providerId);
+
+    boolean existsByNickname(String nickname);
 }

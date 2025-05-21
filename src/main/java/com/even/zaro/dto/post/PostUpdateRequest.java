cd6852d775d56331598c6daf80cc96789facb3fc
LJIEUN <eun773c@daum.net>
2025-05-21 17:55:25 +0900

    feat: 스웨거 관련 어노테이션 추가 및 로그인 인증 관련 수정

--- src/main/java/com/even/zaro/dto/post/PostUpdateRequest.java
+++ src/main/java/com/even/zaro/dto/post/PostUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.even.zaro.dto.post;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,15 +9,23 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
+@Schema(description = "게시글 수정 요청 DTO")
 public class PostUpdateRequest {
 
     @NotBlank(message = "제목은 필수입니다.")
+    @Schema(description = "게시글 제목", example = "오늘의 자취 꿀템 추천!")
     private String title;
 
     @NotBlank(message = "내용은 필수입니다.")
+    @Schema(description = "게시글 내용", example = "자취 꿀템 공유해요~")
     private String content;
 
+    @Schema(description = "게시글 태그", example = "TIP")
     private String tag;
+
+    @Schema(description = "이미지 URL 목록", example = "https://s3.bucket.com/image1.png")
     private List<String> imageUrlList;
+
+    @Schema(description = "썸네일 이미지 URL", example = "https://s3.bucket.com/image1.png")
     private String thumbnailUrl;
 }

4f479657ba42855a865d72956dc37089dd5d65aa
nahyun <knahyuk@gmail.com>
2025-05-22 14:11:25 +0900

    feat: 댓글 리스트 조회 컨트롤러

--- src/main/java/com/even/zaro/controller/CommentController.java
+++ src/main/java/com/even/zaro/controller/CommentController.java
@@ -10,6 +10,10 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -31,5 +35,15 @@ public class CommentController {
             @AuthenticationPrincipal JwtUserInfoDto userInfoDto) {
         CommentResponseDto responseDto = commentService.createComment(postId, requestDto, userInfoDto);
         return ResponseEntity.ok(ApiResponse.success("댓글을 작성했습니다.", responseDto));
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public ResponseEntity<ApiResponse<Page<CommentResponseDto>>> readAllComments(
+            @PathVariable Long postId,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.ASC) Pageable pageable,
+            @AuthenticationPrincipal JwtUserInfoDto userInfoDto
+    ) {
+        Page<CommentResponseDto> responseDto = commentService.readAllComments(postId, pageable, userInfoDto);
+        return ResponseEntity.ok(ApiResponse.success("댓글 리스트를 불러왔습니다.", responseDto));
     }
 }

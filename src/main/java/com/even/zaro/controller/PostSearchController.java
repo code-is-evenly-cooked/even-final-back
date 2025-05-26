991a8aa7a55319213a325b9ee424f96af87d1396
nahyun <knahyuk@gmail.com>
2025-05-27 00:31:01 +0900

    feat: es 검색 api 컨트롤러(기존 유지 테스트 후 대체 예정)

--- src/main/java/com/even/zaro/controller/PostSearchController.java
+++ src/main/java/com/even/zaro/controller/PostSearchController.java
@@ -4,6 +4,7 @@ import com.even.zaro.dto.PageResponse;
 import com.even.zaro.dto.post.PostPreviewDto;
 import com.even.zaro.dto.post.PostSearchDto;
 import com.even.zaro.global.ApiResponse;
+import com.even.zaro.service.PostEsSearchService;
 import com.even.zaro.service.PostSearchService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.Map;
 
 @Tag(name = "게시글 검색 API", description = "카테고리 + 키워드 기반 게시글 검색 기능")
@@ -29,6 +31,7 @@ import java.util.Map;
 public class PostSearchController {
 
     private final PostSearchService postSearchService;
+    private final PostEsSearchService postEsSearchService;
 
     @Operation(summary = "게시글 검색", description = "카테고리 및 키워드 기반으로 게시글을 검색합니다.")
     @GetMapping
@@ -42,4 +45,16 @@ public class PostSearchController {
 
         return ResponseEntity.ok(ApiResponse.success("검색 결과입니다.", results));
     }
+    @Operation(summary = "ES 게시글 검색", description = "Elasticsearch 기반의 게시글 검색 결과를 페이징 형태로 제공합니다.")
+    @GetMapping("/es")
+    public ResponseEntity<ApiResponse<PageResponse<PostSearchDto>>> searchEsPosts(
+            @RequestParam("required = false") String category,
+            @RequestParam String keyword,
+            @ParameterObject @PageableDefault(size = 10, sort = "createdAt", direction = org.springframework.data.domain.Sort.Direction.DESC)
+            Pageable pageable
+    ) throws IOException {
+        PageResponse<PostSearchDto> result = postEsSearchService.searchWithPage(category, keyword, pageable);
+        return ResponseEntity.ok(ApiResponse.success("ES 검색 결과입니다." , result));
+    }
+
 }

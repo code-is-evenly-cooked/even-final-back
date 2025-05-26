53f12cc193dd38a1f5d877c8d5773d4327261df2
yoorym-kim <twilight99me@gmail.com>
2025-05-26 17:17:16 +0900

    feat : notificationApiTest 클래스 생성

---
+++ src/test/java/com/even/zaro/notification/notificationApiTest.java
@@ -0,0 +1,13 @@
+package com.even.zaro.notification;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+public class notificationApiTest {
+
+
+}

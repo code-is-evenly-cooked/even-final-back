6e9afabace8e36bf2f9c26f5552497b05a0bbdbb
yoorym-kim <twilight99me@gmail.com>
2025-05-26 21:10:12 +0900

    feat : 30일 지난 알림 스케줄러 삭제 성공케이스 테스트 구현

--- src/test/java/com/even/zaro/global/scheduler/NotificationCleanupSchedulerTest.java
+++ src/test/java/com/even/zaro/global/scheduler/NotificationCleanupSchedulerTest.java
@@ -1,12 +1,72 @@
 package com.even.zaro.global.scheduler;
 
+import com.even.zaro.entity.Notification;
+import com.even.zaro.entity.Provider;
+import com.even.zaro.entity.Status;
+import com.even.zaro.entity.User;
+import com.even.zaro.repository.NotificationRepository;
+import com.even.zaro.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
 public class NotificationCleanupSchedulerTest {
 
+    @Autowired
+    NotificationCleanupScheduler notificationCleanupScheduler;
+
+    @Autowired
+    NotificationRepository notificationRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void 알림_30일_지나면_스케줄러_삭제_성공() {
+        // given
+        User user = createUser("test@even.com");
+
+        // 기준 시각 : 30일 전 새벽 3시
+        LocalDateTime standard = LocalDateTime.now().minusDays(30).withHour(3).withMinute(0).withSecond(0).withNano(0);
+
+        Notification oldNoti = createNotification(user, false, standard.minusMinutes(1));
+        Notification notOldEnoughNoti = createNotification(user, false, standard.plusMinutes(1));
+
+        // when
+        notificationCleanupScheduler.deleteOldNotifications();
+
+        // then
+        assertThat(notificationRepository.findById(oldNoti.getId())).isEmpty();
+        assertThat(notificationRepository.findById(notOldEnoughNoti.getId())).isPresent();
+    }
+
+    private User createUser(String email) {
+        return userRepository.save(User.builder()
+                .email(email)
+                .password("password")
+                .nickname("닉네임")
+                .provider(Provider.LOCAL)
+                .status(Status.ACTIVE)
+                .build());
+    }
+
+    private Notification createNotification(User user, boolean isRead, LocalDateTime createdAt) {
+        Notification noti = Notification.builder()
+                .user(user)
+                .targetId(1L)
+                .type(Notification.Type.FOLLOW)
+                .isRead(isRead)
+                .createdAt(createdAt)
+                .build();
+        return notificationRepository.save(noti);
+    }
 }

1051c237a3cbb75bd038436d41bc75ea879e23bd
LJIEUN <eun773c@daum.net>
2025-05-27 01:39:08 +0900

    ref: 스웨거 예제값 변경

--- src/main/java/com/even/zaro/dto/post/ReportRequestDTO.java
+++ src/main/java/com/even/zaro/dto/post/ReportRequestDTO.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ReportRequestDTO (
 
-    @Schema(description = "신고 사유 ENUM", example = "SPAM")
+    @Schema(description = "신고 사유 ENUM", example = "ETC")
     ReportReasonType reasonType,
 
     @Schema(description = "기타 사유 선택시에만 입력 ETC", example = "전혀 연관 없는 글과 욕설으로 타인의 기분을 망칩니다.")

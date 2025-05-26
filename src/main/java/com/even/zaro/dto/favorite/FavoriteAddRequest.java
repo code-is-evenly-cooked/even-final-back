4ecd0996c53933439e95864ba5901a51584410a3
saaad99 <ehdgnstla@naver.com>
2025-05-26 11:09:17 +0900

    Ref : 장소 저장에 필요한 필드 추가

--- src/main/java/com/even/zaro/dto/favorite/FavoriteAddRequest.java
+++ src/main/java/com/even/zaro/dto/favorite/FavoriteAddRequest.java
@@ -7,9 +7,22 @@ import lombok.Getter;
 @Getter
 @Builder
 public class FavoriteAddRequest {
-    @Schema(description = "장소 ID", example = "3")
-    private long placeId;
+
+    @Schema(description = "카카오지도 장소 Id", example = "314222")
+    private long kakaoPlaceId;
 
     @Schema(description = "메모", example = "친구랑 가보고 싶은 감성카페")
     private String memo;
+
+    @Schema(description = "장소 이름", example = "강남 삼겹살 집")
+    private String placeName;
+
+    @Schema(description = "주소", example = "강남의 삼겹살집 512")
+    private String address;
+
+    @Schema(description = "위도", example = "37.55123")
+    private double lat;
+
+    @Schema(description = "경도", example = "127.1234455")
+    private double lng;
 }

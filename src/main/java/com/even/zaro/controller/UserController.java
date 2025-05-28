8cc9b255263f248a0d32f5de464bbf6854151f5a
nahyun <knahyuk@gmail.com>
2025-05-28 15:47:43 +0900

    feat: 비밀번호 변경 컨트롤러

--- src/main/java/com/even/zaro/controller/UserController.java
+++ src/main/java/com/even/zaro/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.even.zaro.controller;
 
 import com.even.zaro.dto.jwt.JwtUserInfoDto;
+import com.even.zaro.dto.user.UpdatePasswordRequestDto;
 import com.even.zaro.dto.user.UserInfoResponseDto;
 import com.even.zaro.global.ApiResponse;
 import com.even.zaro.jwt.JwtUtil;
@@ -12,9 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Users", description = "사용자 관련 API")
 @RestController
@@ -22,12 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
-    private final JwtUtil jwtUtil;
 
     @Operation(summary = "내 정보 조회", description = "AccessToken 기반으로 로그인한 사용자의 정보를 조회합니다.", security = {@SecurityRequirement(name = "bearer-key")})
     @GetMapping("/my")
     public ResponseEntity<ApiResponse<UserInfoResponseDto>> getMyInfo(@AuthenticationPrincipal JwtUserInfoDto userInfoDto) {
         UserInfoResponseDto responseDto = userService.getMyInfo(userInfoDto.getUserId());
         return ResponseEntity.ok(ApiResponse.success("내 정보 조회에 성공했습니다.", responseDto));
+    }
+    
+    @PostMapping("/password")
+    public ResponseEntity<ApiResponse<Void>> updatePassword(
+            @RequestBody UpdatePasswordRequestDto requestDto,
+            @AuthenticationPrincipal JwtUserInfoDto userInfoDto) {
+        userService.updatePassword(userInfoDto.getUserId(), requestDto);
+        return ResponseEntity.ok(ApiResponse.success("비밀번호가 변경되었습니다."));
     }
 }

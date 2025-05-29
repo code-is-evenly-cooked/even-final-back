70b5ca6e6a3675d08ce3f343ab6e5177bd3263b4
nahyun <knahyuk@gmail.com>
2025-05-29 16:16:01 +0900

    chore: 닉네임변경 요청, 응답 dto 스웨거 추가

--- src/main/java/com/even/zaro/dto/user/UpdateNicknameResponseDto.java
+++ src/main/java/com/even/zaro/dto/user/UpdateNicknameResponseDto.java
@@ -1,5 +1,6 @@
 package com.even.zaro.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +10,9 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class UpdateNicknameResponseDto {
 
+    @Schema(description = "변경된 닉네임", example = "새닉네임")
     private String nickname;
+
+    @Schema(description = "다음 닉네임 변경 가능 일시", example = "2025-06-13T12:00:00")
     private LocalDateTime nextAvailableChangeDate;
 }

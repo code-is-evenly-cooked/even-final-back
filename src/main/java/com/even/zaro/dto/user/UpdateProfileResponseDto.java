841905bbe4901a86378367d03826d2b34f87e344
nahyun <knahyuk@gmail.com>
2025-05-29 21:29:12 +0900

    feat: 프로필 변경 요청, 응답 dto

---
+++ src/main/java/com/even/zaro/dto/user/UpdateProfileResponseDto.java
@@ -0,0 +1,26 @@
+package com.even.zaro.dto.user;
+
+import com.even.zaro.entity.Gender;
+import com.even.zaro.entity.Mbti;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class UpdateProfileResponseDto {
+
+    @Schema(description = "생일", example = "1997-05-15", nullable = true)
+    private LocalDate birthday;
+
+    @Schema(description = "자취 시작일", example = "2024-01-01", nullable = true)
+    private LocalDate liveAloneDate;
+
+    @Schema(description = "성별", example = "MALE", nullable = true)
+    private Gender gender;
+
+    @Schema(description = "MBTI", example = "INFP", nullable = true)
+    private Mbti mbti;
+}

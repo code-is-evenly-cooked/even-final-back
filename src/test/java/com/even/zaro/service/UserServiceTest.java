42b99320020e762ea0b23f3ab30efda52bee8df7
nahyun <knahyuk@gmail.com>
2025-05-29 15:35:11 +0900

    test: 단위테스트 - 성공 테스트 및 예외처리 7개

--- src/test/java/com/even/zaro/service/UserServiceTest.java
+++ src/test/java/com/even/zaro/service/UserServiceTest.java
@@ -1,5 +1,7 @@
 package com.even.zaro.service;
 
+import com.even.zaro.dto.user.UpdateNicknameRequestDto;
+import com.even.zaro.dto.user.UpdateNicknameResponseDto;
 import com.even.zaro.dto.user.UpdatePasswordRequestDto;
 import com.even.zaro.entity.Status;
 import com.even.zaro.entity.User;
@@ -15,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,6 +34,103 @@ class UserServiceTest {
 
     @Mock
     private PasswordEncoder passwordEncoder;
+
+    @Nested
+    class updateNicknameTest {
+        private User user;
+
+        static User createTestUser() {
+            return User.builder()
+                    .id(1L)
+                    .status(Status.ACTIVE)
+                    .nickname("기존닉네임")
+                    .build();
+        }
+
+        @BeforeEach
+        void setUp() {
+            user = createTestUser();
+            when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        }
+
+        @Test
+        void successfully_updated_nickname() {
+            UpdateNicknameRequestDto requestDto = new UpdateNicknameRequestDto("새닉네임");
+
+            UpdateNicknameResponseDto responseDto = userService.updateNickname(1L, requestDto);
+
+            assertEquals("새닉네임", responseDto.getNickname());
+            assertNotNull(user.getLastNicknameUpdatedAt());
+        }
+
+        @Test
+        void shouldThrowException_whenUserDoesNotExist() {
+            UpdateNicknameRequestDto requestDto = new UpdateNicknameRequestDto("새닉네임");
+            when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+            UserException ex = assertThrows(UserException.class, () -> userService.updateNickname(1L, requestDto));
+
+            assertEquals(ErrorCode.USER_NOT_FOUND, ex.getErrorCode());
+        }
+
+        @Test
+        void shouldThrowException_whenUserIsNotVerified() {
+            user.changeStatus(Status.PENDING);
+            when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+            UpdateNicknameRequestDto requestDto = new UpdateNicknameRequestDto("새닉네임");
+
+            UserException ex = assertThrows(UserException.class, () -> userService.updateNickname(1L, requestDto));
+
+            assertEquals(ErrorCode.MAIL_NOT_VERIFIED, ex.getErrorCode());
+        }
+
+        @Test
+        void shouldThrowException_whenNewNicknameIsBlank() {
+            UpdateNicknameRequestDto requestDto = new UpdateNicknameRequestDto("");
+
+            UserException ex = assertThrows(UserException.class, () -> userService.updateNickname(1L, requestDto));
+
+            assertEquals(ErrorCode.NEW_NICKNAME_REQUIRED, ex.getErrorCode());
+        }
+
+        @Test
+        void shouldThrowException_whenNicknameFormatIsInvalid() {
+            UpdateNicknameRequestDto requestDto = new UpdateNicknameRequestDto("형식파괴!!!");
+
+            UserException ex = assertThrows(UserException.class, () -> userService.updateNickname(1L, requestDto));
+
+            assertEquals(ErrorCode.INVALID_NICKNAME_FORMAT, ex.getErrorCode());
+        }
+
+        @Test
+        void shouldThrowException_whenNicknameAlreadyExists() {
+            UpdateNicknameRequestDto requestDto = new UpdateNicknameRequestDto("이미있니");
+            when(userRepository.existsByNickname("이미있니")).thenReturn(true);
+
+            UserException ex = assertThrows(UserException.class, () -> userService.updateNickname(1L, requestDto));
+
+            assertEquals(ErrorCode.NICKNAME_ALREADY_EXISTED, ex.getErrorCode());
+        }
+
+        @Test
+        void shouldThrowException_whenNewNicknameIsSameAsOriginalOne() {
+            UpdateNicknameRequestDto requestDto = new UpdateNicknameRequestDto("기존닉네임");
+
+            UserException ex = assertThrows(UserException.class, () -> userService.updateNickname(1L, requestDto));
+
+            assertEquals(ErrorCode.NEW_NICKNAME_EQUALS_ORIGINAL_NICKNAME, ex.getErrorCode());
+        }
+
+        @Test
+        void shouldThrowException_whenNicknameChangedWithinRestrictionPeriod() {
+            user.updateLastNicknameUpdatedAt(LocalDateTime.now().minusDays(13));
+            UpdateNicknameRequestDto requestDto = new UpdateNicknameRequestDto("새닉네임");
+
+            UserException ex = assertThrows(UserException.class, () -> userService.updateNickname(1L, requestDto));
+
+            assertEquals(ErrorCode.NICKNAME_UPDATE_COOLDOWN, ex.getErrorCode());
+        }
+    }
 
     @Nested
     class updatePasswordTest {

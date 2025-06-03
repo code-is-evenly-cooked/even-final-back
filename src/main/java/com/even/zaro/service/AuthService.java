4f6d4e4271b451e8e2dbf9e0b378d0f376dcb6d6
nahyun <knahyuk@gmail.com>
2025-06-03 18:43:49 +0900

    feat: 로그아웃 로직
    (cherry picked from commit c9c0e59775ba01042cbb18a34f71ab8744068988)
    
    # Conflicts:
    #	src/main/java/com/even/zaro/service/AuthService.java

--- src/main/java/com/even/zaro/service/AuthService.java
+++ src/main/java/com/even/zaro/service/AuthService.java
@@ -17,6 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 @Slf4j
@@ -158,6 +160,19 @@ public class AuthService {
         String newAccessToken = jwtUtil.generateAccessToken(new JwtUserInfoDto(user.getId()));
 
         return new RefreshResponseDto(newAccessToken);
+    }
+
+    // 로그아웃
+    @Transactional
+    public void signOut(Long userId, String accessToken) {
+        redisTemplate.delete("refresh:" + userId);
+
+        Date expiration = jwtUtil.getAccessTokenExpiration(accessToken);
+        long now = System.currentTimeMillis();
+        long ttl = expiration.getTime() - now;
+
+        redisTemplate.opsForValue()
+                .set("BL:" + accessToken, "logout", ttl, TimeUnit.MILLISECONDS);
     }
 
     public SignInResponseDto generateLoginResponse(User user) {

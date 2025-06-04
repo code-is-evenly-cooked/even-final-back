335e5052995f71740a1b16f7b43cb602341714eb
yoorym-kim <twilight99me@gmail.com>
2025-06-04 19:14:11 +0900

    ref : CommentListener는 publishEvent()하는 트리거 역할로만, NotificationListener에서 handleComment로 해당 이벤트 핸들링

--- src/main/java/com/even/zaro/listener/CommentListener.java
+++ src/main/java/com/even/zaro/listener/CommentListener.java
@@ -2,9 +2,11 @@ package com.even.zaro.listener;
 
 import com.even.zaro.config.SpringContext;
 import com.even.zaro.entity.Comment;
+import com.even.zaro.event.CommentCreatedEvent;
 import com.even.zaro.service.NotificationService;
 import jakarta.persistence.PostPersist;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,11 +14,10 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CommentListener {
 
-    @PostPersist // Comment 엔티티 DB 저장 직후 자동 실행
-    @Transactional
-    public void onCommentCreated(Comment comment) {
+    private final ApplicationEventPublisher eventPublisher;
 
-        NotificationService notificationService = SpringContext.getBean(NotificationService.class);
-        notificationService.createCommentNotification(comment);
+    @PostPersist // Comment 엔티티 DB 저장 직후 자동 실행
+    public void onCommentCreated(Comment comment) {
+        eventPublisher.publishEvent(new CommentCreatedEvent(comment));
     }
 }
--- src/main/java/com/even/zaro/listener/NotificationListener.java
+++ src/main/java/com/even/zaro/listener/NotificationListener.java
@@ -1,4 +1,19 @@
 package com.even.zaro.listener;
 
+import com.even.zaro.event.CommentCreatedEvent;
+import com.even.zaro.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
 public class NotificationListener {
+    private final NotificationService notificationService;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleComment(CommentCreatedEvent event) {
+        notificationService.createCommentNotification(event.getComment());
+    }
 }

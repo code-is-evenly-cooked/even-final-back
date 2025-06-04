873f5fe5ea83e83be73ea210a674e38c10cc21c2
nahyun <knahyuk@gmail.com>
2025-06-04 16:06:38 +0900

    fix: jwt 엔트리포인트 공통 응답 -> 에러 응답 수정

--- src/main/java/com/even/zaro/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/even/zaro/jwt/JwtAuthenticationEntryPoint.java
@@ -1,7 +1,7 @@
 package com.even.zaro.jwt;
 
-import com.even.zaro.global.ApiResponse;
 import com.even.zaro.global.ErrorCode;
+import com.even.zaro.global.ErrorResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -22,7 +22,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
 
-        ApiResponse<Object> body = ApiResponse.fail(ErrorCode.AUTH_REQUIRED);
-        objectMapper.writeValue(response.getWriter(), body);
+        ErrorResponse errorResponse = ErrorResponse.fail(ErrorCode.AUTH_REQUIRED);
+        objectMapper.writeValue(response.getWriter(), errorResponse);
     }
 }

bd00817cdec6f6bcb0e6727bff2d9642ad9948a3
nahyun <knahyuk@gmail.com>
2025-06-04 16:04:11 +0900

    fix: 불필요 import 삭제, allArgsCon 애노테이션으로 대체

--- src/main/java/com/even/zaro/global/ApiResponse.java
+++ src/main/java/com/even/zaro/global/ApiResponse.java
@@ -1,21 +1,15 @@
 package com.even.zaro.global;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
+@AllArgsConstructor
 public class ApiResponse<T> {
 
     private String code;
     private String message;
     private T data;
-
-    private ApiResponse(String code, String message, T data){
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
 
     public static <T> ApiResponse<T> success(String message) {
         return new ApiResponse<>("SUCCESS", message, null);

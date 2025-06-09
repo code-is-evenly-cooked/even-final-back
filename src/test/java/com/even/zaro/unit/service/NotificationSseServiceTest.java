6cc491351e6ed133ea3758fb36405cc36078d517
yoorym-kim <twilight99me@gmail.com>
2025-06-09 21:17:18 +0900

    feat : connect_호출시_emitter_등록되고_초기이벤트_전송시도() 테스트케이스 구현

--- src/test/java/com/even/zaro/unit/service/NotificationSseServiceTest.java
+++ src/test/java/com/even/zaro/unit/service/NotificationSseServiceTest.java
@@ -2,9 +2,13 @@ package com.even.zaro.unit.service;
 
 import com.even.zaro.service.NotificationSseService;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class NotificationSseServiceTest {
@@ -12,9 +16,16 @@ public class NotificationSseServiceTest {
     @InjectMocks
     private NotificationSseService notificationSseService;
 
+    private final Long userId = 1L;
+
     @Nested
     class ConnectTest {
+        @Test
+        void connect_호출시_emitter_등록되고_초기이벤트_전송시도() {
+            SseEmitter emitter = notificationSseService.connect(userId);
 
+            assertThat(emitter).isNotNull();
+        }
     }
 
     @Nested

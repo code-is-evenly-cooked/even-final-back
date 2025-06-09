0ac7af8b9d491dba06341e30f7db988c677d0905
yoorym-kim <twilight99me@gmail.com>
2025-06-09 21:11:46 +0900

    feat : NotificationSseServiceTest 생성, ConnectTest, SendTest nested클래스 정의

---
+++ src/test/java/com/even/zaro/unit/service/NotificationSseServiceTest.java
@@ -0,0 +1,24 @@
+package com.even.zaro.unit.service;
+
+import com.even.zaro.service.NotificationSseService;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class NotificationSseServiceTest {
+
+    @InjectMocks
+    private NotificationSseService notificationSseService;
+
+    @Nested
+    class ConnectTest {
+
+    }
+
+    @Nested
+    class SendTest {
+
+    }
+}

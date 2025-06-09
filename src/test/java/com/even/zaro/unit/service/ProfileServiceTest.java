819d7d7467df59c5d704504a02d8d94289b5be6b
yoorym-kim <twilight99me@gmail.com>
2025-06-09 23:44:16 +0900

    feat : ProfileServiceTest 생성

---
+++ src/test/java/com/even/zaro/unit/service/ProfileServiceTest.java
@@ -0,0 +1,10 @@
+package com.even.zaro.unit.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ProfileServiceTest {
+
+
+}

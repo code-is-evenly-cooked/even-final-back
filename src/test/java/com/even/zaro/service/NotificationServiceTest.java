bb8a8eacc6fb838b15f35ea698b2ba5e09d21165
yoorym-kim <twilight99me@gmail.com>
2025-06-09 15:52:06 +0900

    fix : 테스트클래스명, 파일이름 오타 수정

--- src/test/java/com/even/zaro/service/NotificationServiceTest.java
+++ src/test/java/com/even/zaro/service/NotificationServiceTest.java
@@ -21,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class NotificaitonServiceTest {
+public class NotificationServiceTest {
 
     @InjectMocks
     private NotificationService notificationService;

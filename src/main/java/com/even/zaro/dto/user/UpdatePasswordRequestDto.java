39d8f4c3ae0297f9137223701d1a9377021e9af2
nahyun <knahyuk@gmail.com>
2025-06-12 01:02:34 +0900

    feat: 비밀번호 변경 dto에 현재 비밀번호 not blank / 새 비밀번호 not blank, validNickname 어노테이션 추가

--- src/main/java/com/even/zaro/dto/user/UpdatePasswordRequestDto.java
+++ src/main/java/com/even/zaro/dto/user/UpdatePasswordRequestDto.java
@@ -1,6 +1,8 @@
 package com.even.zaro.dto.user;
 
+import com.even.zaro.global.validator.annotation.ValidPassword;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,8 +11,11 @@ import lombok.Getter;
 public class UpdatePasswordRequestDto {
 
     @Schema(description = "현재 비밀번호", example = "Old1234!")
+    @NotBlank(message = "CURRENT_PASSWORD_REQUIRED")
     private String currentPassword;
 
     @Schema(description = "새 비밀번호", example = "New1234!")
+    @NotBlank(message = "PASSWORD_REQUIRED")
+    @ValidPassword
     private String newPassword;
 }

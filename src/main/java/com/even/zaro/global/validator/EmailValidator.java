0803389ba9db96501ae07600979f5b513416b118
nahyun <knahyuk@gmail.com>
2025-06-12 00:00:48 +0900

    feat: Email Validator - Pattern 검사 클래스

---
+++ src/main/java/com/even/zaro/global/validator/EmailValidator.java
@@ -0,0 +1,17 @@
+package com.even.zaro.global.validator;
+
+import com.even.zaro.global.validator.annotation.ValidEmail;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Pattern;
+
+public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
+
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return value != null && Pattern.matches(EMAIL_REGEX, value);
+    }
+}

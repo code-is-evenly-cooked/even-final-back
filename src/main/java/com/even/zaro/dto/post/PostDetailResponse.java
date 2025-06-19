98474cdcd2823f7f92ed397bed79fac9fae5e78f
LJIEUN <eun773c@daum.net>
2025-06-19 16:42:52 +0900

    ref: @Named 사용으로 리스트조회에만 적용

--- src/main/java/com/even/zaro/dto/post/PostDetailResponse.java
+++ src/main/java/com/even/zaro/dto/post/PostDetailResponse.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
@@ -13,6 +14,7 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @Builder
+@NoArgsConstructor
 @Schema(description = "게시글 상세 조회 응답 DTO")
 public class PostDetailResponse {
 
